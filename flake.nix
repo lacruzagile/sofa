61b33ec1a7f42726e94788b0a3d63d0d37a6ef68
Robert Helgesson <robert.helgesson@sinch.com>
2021-12-01 11:21:31 +0100

    Add GitLab CI
    Also switch to using Nix Flakes for setting up a development shell.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A prototype front-end of Smart § Spec";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShellNoCC {
+          name = "nix-shell-for-sofa";
+          buildInputs = with pkgs; [
+            dhall-lsp-server
+            nodePackages.purescript-language-server
+            nodePackages.purty
+            nodejs
+            purescript
+            spago
+            yarn
+          ];
+        };
+      });
+}
