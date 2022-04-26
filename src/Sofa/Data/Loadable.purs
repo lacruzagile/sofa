2a9b8da71f9cdeff255bd5b68e91d78cd2c14e57
Robert Helgesson <robert.helgesson@sinch.com>
2022-04-26 17:36:12 +0200

    Remove use of simple-ajax
    Instead simply use affjax directly. It's not much more code and we can
    do things like handling timeouts.

--- src/Sofa/Data/Loadable.purs
+++ src/Sofa/Data/Loadable.purs
@@ -13,14 +13,17 @@ module Sofa.Data.Loadable
   ) where
 
 import Prelude
-import Affjax (printError)
-import Data.Argonaut (class DecodeJson, class EncodeJson, printJsonDecodeError)
+import Affjax as AX
+import Affjax.RequestBody as RequestBody
+import Affjax.RequestHeader (RequestHeader)
+import Affjax.ResponseFormat as ResponseFormat
+import Affjax.StatusCode (StatusCode(..))
+import Data.Argonaut (class DecodeJson, class EncodeJson, Json, decodeJson, encodeJson, printJsonDecodeError)
 import Data.Either (Either(..))
+import Data.HTTP.Method as HTTP
 import Data.Maybe (Maybe(..))
-import Data.Variant (default, on)
+import Data.Time.Duration (Milliseconds(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
-import Simple.Ajax (_affjaxError, _notFound, _parseError)
-import Simple.Ajax as AJX
 import Sofa.Data.Auth (class CredentialStore, getAuthorizationHeader)
 
 -- | A representation of URL data that is loaded. The load can, e.g., fail with
@@ -74,20 +77,57 @@ toMaybe = case _ of
   Loaded x -> Just x
   _ -> Nothing
 
+handleResponse ::
+  forall result body r.
+  (body -> Loadable result) ->
+  Either AX.Error { body :: body, status :: StatusCode | r } ->
+  Loadable result
+handleResponse handleBody = case _ of
+  Left err -> Error $ AX.printError err
+  Right resp
+    | statusOk resp.status -> handleBody resp.body
+    | statusNotFound resp.status -> Error "Not found"
+    | otherwise -> Error "Generic error"
+  where
+  statusOk (StatusCode n) = 200 <= n && n < 300
+
+  statusNotFound (StatusCode n) = n == 404
+
+handleEmptyResponse :: Either AX.Error (AX.Response Unit) -> Loadable Unit
+handleEmptyResponse = handleResponse \_ -> Loaded unit
+
+handleJsonResponse ::
+  forall a.
+  DecodeJson a =>
+  Either AX.Error (AX.Response Json) -> Loadable a
+handleJsonResponse =
+  handleResponse \body -> case decodeJson body of
+    Left err -> Error $ printJsonDecodeError err
+    Right value -> Loaded value
+
+withAuthorizationHeader ::
+  forall m a.
+  MonadAff m =>
+  CredentialStore m =>
+  (RequestHeader -> m (Loadable a)) -> m (Loadable a)
+withAuthorizationHeader fetcher = do
+  creds <- getAuthorizationHeader
+  case creds of
+    Left err -> pure $ Error err
+    Right authHdr -> fetcher authHdr
+
 -- | Fetch JSON from an URL.
 getJson :: forall a m. DecodeJson a => MonadAff m => String -> m (Loadable a)
-getJson url = do
-  res <- liftAff $ AJX.get url
-  pure
-    $ case res of
-        Left error ->
-          Error
-            $ default "Generic error"
-            # on _affjaxError printError
-            # on _notFound (const "Not found")
-            # on _parseError printJsonDecodeError
-            $ error
-        Right content -> Loaded content
+getJson url =
+  liftAff
+    $ map handleJsonResponse
+    $ AX.request
+    $ AX.defaultRequest
+        { url = url
+        , method = Left HTTP.GET
+        , timeout = Just (Milliseconds 10_000.0)
+        , responseFormat = ResponseFormat.json
+        }
 
 -- | Fetch JSON from an URL.
 getRJson ::
@@ -96,22 +136,18 @@ getRJson ::
   MonadAff m =>
   CredentialStore m =>
   String -> m (Loadable a)
-getRJson url = do
-  creds <- getAuthorizationHeader
-  case creds of
-    Left err -> pure $ Error err
-    Right authHdr -> do
-      res <- liftAff $ AJX.getR { headers: [ authHdr ] } url
-      pure
-        $ case res of
-            Left error ->
-              Error
-                $ default "Generic error"
-                # on _affjaxError printError
-                # on _notFound (const "Not found")
-                # on _parseError printJsonDecodeError
-                $ error
-            Right content -> Loaded content
+getRJson url =
+  withAuthorizationHeader \authHdr -> do
+    liftAff
+      $ map handleJsonResponse
+      $ AX.request
+      $ AX.defaultRequest
+          { url = url
+          , method = Left HTTP.GET
+          , headers = [ authHdr ]
+          , timeout = Just (Milliseconds 10_000.0)
+          , responseFormat = ResponseFormat.json
+          }
 
 -- | Submit JSON using a PATCH request and parse the response.
 patchRJson ::
@@ -123,27 +159,19 @@ patchRJson ::
   String ->
   a ->
   m (Loadable b)
-patchRJson url body = do
-  creds <- getAuthorizationHeader
-  case creds of
-    Left err -> pure $ Error err
-    Right authHdr -> do
-      res <-
-        liftAff
-          $ AJX.patchR
-              { headers: [ authHdr ] }
-              url
-              (Just body)
-      pure
-        $ case res of
-            Left error ->
-              Error
-                $ default "Generic error"
-                # on _affjaxError printError
-                # on _notFound (const "Not found")
-                # on _parseError printJsonDecodeError
-                $ error
-            Right content -> Loaded content
+patchRJson url body =
+  withAuthorizationHeader \authHdr ->
+    liftAff
+      $ map handleJsonResponse
+      $ AX.request
+      $ AX.defaultRequest
+          { url = url
+          , method = Left HTTP.PATCH
+          , headers = [ authHdr ]
+          , timeout = Just (Milliseconds 10_000.0)
+          , responseFormat = ResponseFormat.json
+          , content = Just $ RequestBody.json $ encodeJson body
+          }
 
 -- | Submit JSON using a POST request and parse the response.
 postRJson ::
@@ -155,27 +183,19 @@ postRJson ::
   String ->
   a ->
   m (Loadable b)
-postRJson url body = do
-  creds <- getAuthorizationHeader
-  case creds of
-    Left err -> pure $ Error err
-    Right authHdr -> do
-      res <-
-        liftAff
-          $ AJX.postR
-              { headers: [ authHdr ] }
-              url
-              (Just body)
-      pure
-        $ case res of
-            Left error ->
-              Error
-                $ default "Generic error"
-                # on _affjaxError printError
-                # on _notFound (const "Not found")
-                # on _parseError printJsonDecodeError
-                $ error
-            Right content -> Loaded content
+postRJson url body =
+  withAuthorizationHeader \authHdr ->
+    liftAff
+      $ map handleJsonResponse
+      $ AX.request
+      $ AX.defaultRequest
+          { url = url
+          , method = Left HTTP.POST
+          , headers = [ authHdr ]
+          , timeout = Just (Milliseconds 10_000.0)
+          , responseFormat = ResponseFormat.json
+          , content = Just $ RequestBody.json $ encodeJson body
+          }
 
 -- | Submit an empty POST request and parse the response.
 postRJson_ ::
@@ -185,22 +205,18 @@ postRJson_ ::
   CredentialStore m =>
   String ->
   m (Loadable a)
-postRJson_ url = do
-  creds <- getAuthorizationHeader
-  case creds of
-    Left err -> pure $ Error err
-    Right authHdr -> do
-      res <- liftAff $ AJX.postR { headers: [ authHdr ] } url (Nothing :: Maybe Int)
-      pure
-        $ case res of
-            Left error ->
-              Error
-                $ default "Generic error"
-                # on _affjaxError printError
-                # on _notFound (const "Not found")
-                # on _parseError printJsonDecodeError
-                $ error
-            Right content -> Loaded content
+postRJson_ url =
+  withAuthorizationHeader \authHdr ->
+    liftAff
+      $ map handleJsonResponse
+      $ AX.request
+      $ AX.defaultRequest
+          { url = url
+          , method = Left HTTP.POST
+          , headers = [ authHdr ]
+          , timeout = Just (Milliseconds 10_000.0)
+          , responseFormat = ResponseFormat.json
+          }
 
 -- | Submit a DELETE request and parse the response.
 deleteR_ ::
@@ -209,18 +225,14 @@ deleteR_ ::
   CredentialStore m =>
   String ->
   m (Loadable Unit)
-deleteR_ url = do
-  creds <- getAuthorizationHeader
-  case creds of
-    Left err -> pure $ Error err
-    Right authHdr -> do
-      res <- liftAff $ AJX.deleteR_ { headers: [ authHdr ] } url
-      pure
-        $ case res of
-            Left error ->
-              Error
-                $ default "Generic error"
-                # on _affjaxError printError
-                # on _notFound (const "Not found")
-                $ error
-            Right content -> Loaded content
+deleteR_ url =
+  withAuthorizationHeader \authHdr ->
+    liftAff
+      $ map handleEmptyResponse
+      $ AX.request
+      $ AX.defaultRequest
+          { url = url
+          , method = Left HTTP.DELETE
+          , headers = [ authHdr ]
+          , timeout = Just (Milliseconds 10_000.0)
+          }
