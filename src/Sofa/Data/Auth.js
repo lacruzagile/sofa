2602f7f6c283d97ca32d53a60674c28da436c852
Robert Helgesson <robert.helgesson@sinch.com>
2022-06-23 11:19:29 +0200

    Prepare for production deploy

--- src/Sofa/Data/Auth.js
+++ src/Sofa/Data/Auth.js
@@ -1,8 +1,20 @@
 "use strict";
 
 exports.ssoBaseUrl = () => {
-  if (typeof __DEPLOYMENT_ENV__ == 'undefined' || __DEPLOYMENT_ENV__ == 'staging') {
-    return 'https://sso.admin-portal.int.staging.sinch.com'
+  const urlStaging = 'https://sso.admin-portal.int.staging.sinch.com';
+  const urlProduction = 'https://sso.int.clxnetworks.net';
+
+  // If we are deployed in Kubernetes then determine the environment by simply checking the URL.
+  if (typeof __DEPLOYMENT_ENV__ == 'undefined') {
+    if (window.location.hostname == 'sofa.eu1.bpa.unauth.int.prod.sinch.com') {
+      return urlProduction;
+    } else {
+      return urlStaging;
+    }
+  } else if (__DEPLOYMENT_ENV__ == 'staging') {
+    return urlStaging;
+  } else if (__DEPLOYMENT_ENV__ == 'production') {
+    return urlProduction;
   } else {
     throw `unknown deployment environment ${__DEPLOYMENT_ENV__}`
   }
