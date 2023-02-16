e1b36baf3f1cf81b9378092dab7667c1ae01a12f
AD\luiaya <luis.gonzalo.ayala.cruz@sinch.com>
2023-02-16 09:52:25 -0600

    BPA-721 Added user guide button

--- src/Sofa/App/Router.purs
+++ src/Sofa/App/Router.purs
@@ -260,15 +260,22 @@ renderNavbar state =
     , "items-center"
     ]
 
-  primaryItem route =
+  primaryItem route = 
     HH.a
       [ Route.href route
       , Css.classes [ "flex", "w-64", "h-full" ]
       ]
       [ Icon.sinchLogo
-          [ Icon.classes [ Css.c "ml-6", Css.c "my-auto", Css.c "h-6" ]
+          [ Icon.classes [ Css.c "ml-6", Css.c "my-auto", Css.c "h-4" ]
           , Icon.ariaLabel "SOFA Home"
           ]
+        ,HH.a
+          [
+            HP.href "https://platform.int.sinch.com/docs/provisioning/sofa-ui/"
+            ,HP.target "_blank"
+            ,Css.classes [ "align-middle", "my-auto"]
+          ]
+          [ HH.button [ Css.classes [ "align-middle","nectary-btn-secondary", "h-8", "ml-6"]][ HH.text "SOFA User Guide"]]
       ]
 
   tileMenu = HH.slot_ NavbarTile.proxy unit NavbarTile.component absurd
